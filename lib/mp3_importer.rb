4eeafc78546537ca274e271043f3aff2c077105b
Christopher Garcia <christopher.garcia08@gmail.com>
2021-03-03 01:33:27 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,21 +1,15 @@
-class Song
-  attr_accessor :name, :artist 
+class MP3Importer
+  attr_accessor :path 
   
-  def initialize(name)
-    @name = name
+  def initialize(path)
+    @path = path
   end
   
-  def self.new_by_filename(file)
-    artist.name = file.split(" - ")[0]
-    song_name = file.split(" - ")[1]
-    song = Song.new(song_name)
-    song.artist_name = artist_name
-    song
+  def files
+    Dir[@path+"/*.mp3].map { |file| file.split("/").last }
   end
   
-  def artist_name=(name)
-    artist = Artist.find_or_create_by_name(name)
-    self.artist = artist
-    artist.add_song(self)
+  def import 
+    files.each { |file| Song.new_by_filename(file) }
   end
 end
