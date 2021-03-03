15f52480843bb0e760639fafb892f18fd628fb34
Christopher Garcia <christopher.garcia08@gmail.com>
2021-03-03 01:43:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,27 +1,35 @@
+require 'pry'
+
 class Song
-  attr_accessor :name, :artist
 
   @@all = []
 
+  attr_accessor :name, :artist
+
   def initialize(name)
     @name = name
+    @@all << self
   end
 
-  def self.new_by_filename(file)
-    artist_name = file.split(" - ")[0]
-    song_name = file.split(" - ")[1]
-    song = Song.new(song_name)
-    song.artist_name = artist_name
-    song
-  end
-
-  def artist_name=(name)
-    artist = Artist.find_or_create_by_name(name)
-    self.artist = artist
-    artist.add_song(self)
-  end
-  
   def self.all
     @@all
   end
-end 
+
+def self.new_by_filename(filename)
+  #binding.pry
+  file_parsed = filename.split(/ - (.+) - (.+).mp3/)
+
+  song = Song.new(file_parsed[1])
+  song.artist = Artist.find_or_create_by_name(file_parsed[0])
+  song
+end
+
+def artist_name=(artist)
+  self.artist = Artist.find_or_create_by_name(artist)
+end
+
+
+
+
+
+end
