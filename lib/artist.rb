d4d43a2e93837cc2bac6eb944cf4085ab702a32a
Christopher Garcia <christopher.garcia08@gmail.com>
2021-03-03 01:21:36 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,21 +1,28 @@
 class Artist
   
-  attr_accessor :name
+  attr_accessor :name, :songs
  
   @@all = []
  
   def initialize(name)
     @name = name
+    @songs = []
+    save
   end
  
  def self.all
    @@all
  end
  
- def add_song(song)
-   self.songs << song
+ def self.create(name)
+   artist = self.new(name)
+   artist.name = name
+   artist
  end
  
- def songs()
+ def self.find(name)
+   self.all.detect { |artist| artist.name == name }
+ end
  
+ def self.find
 end
